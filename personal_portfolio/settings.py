e63a012653be91db8cecd8f80f63e3103a085e18
a24luissr <a24luissr@iessanclemente.net>
2024-12-04 09:42:28 +0000

    hostname fixed

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ap_@cmh*l)oprbtu3xhjeq19bmd#=$yx^@gh^419i^ll*x!$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['a24luissr.pythonanywhere.com']
-
 
 # Application definition
 
